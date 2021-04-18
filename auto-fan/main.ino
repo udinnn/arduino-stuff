973734343c9dc6df882585825f5ebe1d682cb199
Udin Sedunia <74655358+udinnn@users.noreply.github.com>
2021-04-18 15:35:09 +0700

    Create main.ino

---
+++ auto-fan/main.ino
@@ -0,0 +1,30 @@
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 7 // colok digital pin 7 ke dht
+#define RELAY  6 // colok digital pin 6 ke relay
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(RELAY, OUTPUT);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  float suhu = DHT.temperature;
+  float lembab = DHT.humidity;
+  Serial.print("Temperatur = ");
+  Serial.println(suhu);
+  Serial.print("Kelembaban = ");
+  Serial.println(lembab);
+  delay(1000);
+
+  if (suhu < 30.00){
+    digitalWrite(RELAY,LOW);  // nyalain relay
+   }
+   else if(suhu >= 30.00){
+    digitalWrite(RELAY,HIGH); // matiin relay
+   }
+}
